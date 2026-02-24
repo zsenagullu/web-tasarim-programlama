39dbfa36c17a9e791962f7cc8392d3be2c7725b7
zsenagullu <235541048@firat.edu.tr>
2026-02-24 20:36:04 +0300

    chore: initial project setup with Vite + React + TS

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <h1>Web Tasarimi ve Programlama</h1>
+      <h2>LAB-1</h2>
+      <p>Ad Soyad: Zeliha Sena Güllü</p>
+      <p>Ogrenci No: 235541048</p>
+    </div>
+  )
+}
+
+export default App
